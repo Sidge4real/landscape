6348ed318d9052a31ee5f309180fbeed0f7d31f7
Lukas <vanderspiegel.lukas@gmail.com>
2023-08-28 21:38:34 +0200

    cache (helper) updated | if cache folder doesn't exist, it creates a new one

--- helpers/cache.cjs
+++ helpers/cache.cjs
@@ -6,9 +6,16 @@ const mainProjectPath = path.resolve(__dirname, '..');
 const cacheFolderPath = path.join(__dirname, '../data/cache');
 const cachedVersionFilePath = path.join(cacheFolderPath, 'version.dat');
 const packageJsonPath = path.join(mainProjectPath, 'package.json');
+
 // CURRENT
 const packageJson = require(packageJsonPath);
 const currentVersion = packageJson.version;
+
+// Create the cache folder if it doesn't exist
+if (!fs.existsSync(cacheFolderPath)) {
+  fs.mkdirSync(cacheFolderPath, { recursive: true });
+}
+
 // READ
 let previousVersion = '';
 if (fs.existsSync(cachedVersionFilePath)) {
@@ -17,16 +24,17 @@ if (fs.existsSync(cachedVersionFilePath)) {
   // NEW FILE (if not exist)
   fs.writeFileSync(cachedVersionFilePath, currentVersion, 'utf8');
 }
+
 // COMPARE
 if (previousVersion !== currentVersion) {
+  // Clear cache files if the version has changed
   if (fs.existsSync(cacheFolderPath)) {
     fs.readdirSync(cacheFolderPath).forEach(file => {
       const filePath = path.join(cacheFolderPath, file);
       fs.unlinkSync(filePath);
     });
-  } else {
-    fs.mkdirSync(cacheFolderPath, { recursive: true });
   }
-  // NEW
+  
+  // Update the cached version to the current version
   fs.writeFileSync(cachedVersionFilePath, currentVersion, 'utf8');
 }
